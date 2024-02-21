32ee1288b4ea2f8db2c589e662d4e723dd7382ca
Iffy Eleweke <omaleweks@gmail.com>
2024-02-21 10:02:23 -0500

    Created a test that checks if the MessageComponent functions with the data passing to its template

--- Week 7/week-7-day-1/m4-w7-d1-homework/vueapp-test/my-app/tests/unit/example.spec.js
+++ Week 7/week-7-day-1/m4-w7-d1-homework/vueapp-test/my-app/tests/unit/example.spec.js
@@ -1,11 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
+const MessageComponent = {
+  template: '<p>{{ msg }}</p>',
+  props: ['msg']
+}
+
+describe('MessageComponent', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
+    const wrapper = shallowMount(MessageComponent, {
+      propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
   })
